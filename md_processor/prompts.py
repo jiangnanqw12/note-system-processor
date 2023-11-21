9b10db0d0b3f181a39ae727120e5a61104e738a9
shade <382961818@qq.com>
2023-11-21 17:58:39 +0800

    modified:   md_processor/main.py 	modified:   md_processor/prompts.py

--- md_processor/prompts.py
+++ md_processor/prompts.py
@@ -137,6 +137,42 @@ def format_code_2_gpt_input(content=None, copy_to_clipboard=True):
     return repr(content)
 
 
+def format_python_2_gpt_input(content=None, copy_to_clipboard=True):
+    """
+    Formats the input content by replacing multiple newline characters with a single newline character and
+    multiple spaces with a single space. If no content is provided, it fetches the content from the clipboard,
+    formats it, and then copies the formatted content back to the clipboard if copy_to_clipboard is True.
+
+    :param content: The input content to format. If None, the content will be taken from the clipboard.
+    :param copy_to_clipboard: A flag indicating whether to copy the formatted content back to the clipboard.
+    :return: The formatted content as a string.
+    """
+    if content is None:
+        try:
+            content = pyperclip.paste()
+        except pyperclip.PyperclipException:
+            print("Unable to access the clipboard.")
+            return None
+
+    content = content.replace("\r\n", "\n").replace("\\n", "\n")
+
+    replacements = [
+        (r"\n{2,}", "\n"),
+        (r"[ ]{4}", "\t")
+    ]
+
+    for pattern, replacement in replacements:
+        content = re.sub(pattern, replacement, content)
+
+    if copy_to_clipboard:
+        try:
+            pyperclip.copy(repr(content))
+        except pyperclip.PyperclipException:
+            print("Unable to copy content to the clipboard.")
+
+    return repr(content)
+
+
 def format_c_cpp_2_gpt_input(content=None, copy_to_clipboard=True):
     """
     Formats the input content by replacing multiple newline characters with a single newline character and
@@ -213,53 +249,139 @@ I appreciate your assistance. Thank you!
     pyperclip.copy(final_string)
 
 
-def format_code_current_dir(current_dir=None):
+def read_file_skip_non_utf8_parts(file_path):
+    import logging
+    """
+    Reads a file in binary mode and decodes it to UTF-8.
+    Skips the parts of the file that are not UTF-8 encoded.
+
+    :param file_path: Path to the file.
+    :return: Decoded file content with non-UTF-8 parts skipped.
+    """
+    try:
+        with open(file_path, 'rb') as f:
+            byte_content = f.read()
+            return byte_content.decode('utf-8', errors='ignore')
+    except Exception as e:
+        logging.error(f"Error reading file {file_path}: {e}")
+    return None
+
+
+def format_python_current_dir(current_dir=None):
 
     if current_dir is None:
         current_dir = os.getcwd()
-    files_py = [f for f in os.listdir(current_dir) if f.endswith('.py')]
-    output_dir = f'{current_dir}/output'
+    files_python = [f for f in os.listdir(current_dir) if (
+        f.endswith('.py'))]
+    output_dir = os.path.join(current_dir, 'output')
     os.makedirs(output_dir, exist_ok=True)
-    with open(f'{output_dir}/output.md', 'w', encoding="utf-8") as f1:
-        for file in files_py:
-            with open(f'{current_dir}/{file}', 'r', encoding="utf-8") as f:
-                content = f.read()
-            content = format_code_2_gpt_input(
-                content=content, copy_to_clipboard=False)
-            f1.write(content)
+    for root, dirs, files in os.walk(current_dir):
+        for file in files:
+            if file.endswith('.c') or file.endswith('.cpp') or file.endswith('.h') or file.endswith('.hpp'):
+                # Get file name
+                file_name = os.path.basename(file)
+
+                # Get directory name
+                dir_name = os.path.basename(root)
+                print(dir_name)
+                if dir_name.startswith('.'):
+                    continue
+                origin_dir = os.path.join(root, file)
+                # read file skip none utf-8
+
+                content = read_file_skip_non_utf8_parts(origin_dir)
+                content = format_c_cpp_2_gpt_input(
+                    content=content, copy_to_clipboard=False)
+                floder_sep = os.path.join(output_dir, dir_name)
+                os.makedirs(floder_sep, exist_ok=True)
+                file_dir = os.path.join(floder_sep, f'{file_name}.md')
+                total_dir = os.path.join(
+                    output_dir, f'{dir_name}.md')
+
+                with open(file_dir, 'w', encoding="utf-8") as f1:
+                    f1.write(f"\"{file_name}\": \"{content}\",\n")
+                if os.path.exists(total_dir):
+                    with open(total_dir, 'r', encoding="utf-8") as f1:
+                        content1 = f1.read()
+                else:
+                    content1 = ''
+                with open(total_dir, 'w', encoding="utf-8") as f1:
+                    content = f1.write(
+                        content1+f"\"{file_name}\": \"{content}\",\n")
+
     return current_dir
 
 
-def format_c_cpp_current_dir(current_dir=None):
+def format_code_current_dir(current_dir=None):
 
     if current_dir is None:
         current_dir = os.getcwd()
     files_c_cpp = [f for f in os.listdir(current_dir) if (
         f.endswith('.c') or f.endswith('.cpp') or f.endswith('.h') or f.endswith('.hpp'))]
-    output_dir = f'{current_dir}/output'
+    output_dir = os.path.join(current_dir, 'output')
     os.makedirs(output_dir, exist_ok=True)
     for root, dirs, files in os.walk(current_dir):
         for file in files:
             if file.endswith('.c') or file.endswith('.cpp') or file.endswith('.h') or file.endswith('.hpp'):
-                # get file name
-                file_name = file.split('.')[0]
-                # get dir name
-                dir_name = root.split('/')[-1]
+                # Get file name
+                file_name = os.path.basename(file)
+
+                # Get directory name
+                dir_name = os.path.basename(root)
+                print(dir_name)
                 if dir_name.startswith('.'):
                     continue
-                with open(f'{root}/{file}', 'r', encoding="utf-8") as f:
-                    content = f.read()
+                origin_dir = os.path.join(root, file)
+                # read file skip none utf-8
+
+                content = read_file_skip_non_utf8_parts(origin_dir)
                 content = format_c_cpp_2_gpt_input(
                     content=content, copy_to_clipboard=False)
-                file_dir = os.path.join(
-                    output_dir, dir_name, f'{file_name}.md')
+                floder_sep = os.path.join(output_dir, dir_name)
+                os.makedirs(floder_sep, exist_ok=True)
+                file_dir = os.path.join(floder_sep, f'{file_name}.md')
                 total_dir = os.path.join(
                     output_dir, f'{dir_name}.md')
+
                 with open(file_dir, 'w', encoding="utf-8") as f1:
-                    f1.write(content)
-                with open(total_dir, 'r', encoding="utf-8") as f1:
-                    content1 = f1.read()
+                    f1.write(f"\"{file_name}\": \"{content}\",\n")
+                if os.path.exists(total_dir):
+                    with open(total_dir, 'r', encoding="utf-8") as f1:
+                        content1 = f1.read()
+                else:
+                    content1 = ''
                 with open(total_dir, 'w', encoding="utf-8") as f1:
-                    content = f1.write(content1+content)
+                    content = f1.write(
+                        content1+f"\"{file_name}\": \"{content}\",\n")
+            elif (file.endswith('.py')):
+                file_name = os.path.basename(file)
+
+                # Get directory name
+                dir_name = os.path.basename(root)
+                print(dir_name)
+                if dir_name.startswith('.'):
+                    continue
+                origin_dir = os.path.join(root, file)
+                # read file skip none utf-8
+
+                content = read_file_skip_non_utf8_parts(origin_dir)
+                content = format_python_2_gpt_input(
+                    content=content, copy_to_clipboard=False)
+                floder_sep = os.path.join(output_dir, dir_name)
+                os.makedirs(floder_sep, exist_ok=True)
+                file_dir = os.path.join(floder_sep, f'{file_name}.md')
+                total_dir = os.path.join(
+                    output_dir, f'{dir_name}.md')
+
+                with open(file_dir, 'w', encoding="utf-8") as f1:
+                    f1.write(f"\"{file_name}\": \"{content}\",\n")
+                if os.path.exists(total_dir):
+                    with open(total_dir, 'r', encoding="utf-8") as f1:
+                        content1 = f1.read()
+                else:
+                    content1 = ''
+                with open(total_dir, 'w', encoding="utf-8") as f1:
+                    content = f1.write(
+                        content1+f"\"{file_name}\": \"{content}\",\n")
 
     return current_dir
