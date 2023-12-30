46f6cd70aa0fb7b44b7580a40201a909e9cc7cc5
shade <382961818@qq.com>
2023-12-30 11:03:58 +0800

    modified:   md_processor/regex.py

--- md_processor/regex.py
+++ md_processor/regex.py
@@ -1,6 +1,13 @@
 # Regular Expression Utilities
 
-regex_code_block = r"^    ```(\n[\s\S]*?\n)    ```$"
-# r"    ```python$1    ```"
-regex_star_bullet_list_to_dash_list = r"^(\s*)\*"
-# \1-
+pattern_code_block = r"^    ```(\n[\s\S]*?\n)    ```$"
+replacement_code_block = r"    ```python$1    ```"
+pattern_star_bullet_list_to_dash_list = r"^(\s*)\*"
+replacement_star_bullet_list_to_dash_list = r"\1- "
+
+pattern_old_sub = r'\[\[\d{1,2}:(\d{1,2}:\d{1,2}),\d{1,3} --> \d{1,2}:\d{1,2}:\d{1,2},\d{1,3}\],\[(.*)\]\]'
+"""
+[[00:00:00,330 --> 00:00:01,750],[哈喽，大家好，我是王刚。]]
+"""
+replacement_old_sub = r'(\1) \2'
+replacement_old_sub2 = r'($1) $2'
